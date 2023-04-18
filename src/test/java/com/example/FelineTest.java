fbe00046bac852ff4731b4be26517a094337ae7a
Olesya Semenova <mazutka@list.ru>
2023-04-18 12:00:43 +0300

    add jacoco report

---
+++ src/test/java/com/example/FelineTest.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FelineTest {
+
+    @Spy
+    Feline feline;
+    @Test
+    public void eatMeat() throws Exception {
+        List<String> actResult = feline.eatMeat();
+        Mockito.verify(feline, Mockito.times(1)).getFood("Хищник");
+    }
+
+    @Test
+    public void getFamily() {
+        String expResult = "Кошачьи";
+        String actResult = feline.getFamily();
+        assertEquals(expResult,actResult);
+    }
+
+    @Test
+    public void getKittensWithOutParams() {
+        int actResult = feline.getKittens();
+        Mockito.verify(feline,Mockito.times(1)).getKittens(1);
+    }
+
+    @Test
+    public void getKittensWithParams() {
+        int expResult = 5;
+        int actResult = feline.getKittens(5);
+        assertEquals(expResult,actResult);
+    }
+}
